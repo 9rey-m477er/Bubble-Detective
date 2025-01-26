3785a2e60a27f426924def92aa943fa6a451e444
DMossor81 <158850679+DMossor81@users.noreply.github.com>
2025-01-26 00:03:33 -0500

    Location Manager

--- Assets/Scripts/LocationManager.cs
+++ Assets/Scripts/LocationManager.cs
@@ -1,40 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.FilePathAttribute;
 
 public class LocationManager : MonoBehaviour
 {
     public int currentLocation = 0;
     public int currentDialogue = 0;
-    public DialogueController DialogueController;
+    public Image narrowBackground;
+    public Image wideBackground;
+    public DialogueController dialogueController;
+    public LocationObject[] locations;
+    public bool dialoguesFinished = false;
+    public bool isDialogueOpen = false;
+
+    public LocationObject l;
+    public DialogueText t;
+
     public Queue<LocationObject> locationObjects = new Queue<LocationObject>();
+    public Queue<DialogueText> dialogues = new Queue<DialogueText>();
     void Start()
     {
-        
+        for (int i=0; i<locations.Length; i++)
+        {
+            locationObjects.Enqueue(locations[i]);
+        }
+        advanceLocation();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if(DialogueController.conversationEnded == true) //go to next dialogue
-        //{
-        //    currentDialogue++;
-        //    DialogueController.displayNextParagraph(locationObjects[currentLocation].dialogues[currentDialogue]);
-        //}
-        //if(LocationObject.dialogues.Count == currentDialogue)
-        //{
-
-        //}
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (isDialogueOpen)
+            {
+                advanceDialogue(t);
+            }
+        }
     }
 
-    private void loadLocation(LocationObject location)
+    private void advanceLocation()
     {
+        l = locationObjects.Dequeue();
+        if (l.isInvest)
+        {
+            wideBackground.gameObject.SetActive(true);
+            narrowBackground.gameObject.SetActive(false);
+            wideBackground.sprite = l.background;
+        }
+        else
+        {
+            narrowBackground.gameObject.SetActive(true);
+            wideBackground.gameObject.SetActive(false);
+            narrowBackground.sprite = l.background;
+        }
+        loadNextDialogue(l);
+    }
 
+    private void loadDialogues(LocationObject location)
+    {
         for (int i = 0; i < location.dialogues.Length; i++)
         {
+            dialogues.Enqueue(location.dialogues[i]);
+        }        
+    }
 
+    private void loadNextDialogue(LocationObject location)
+    {
+        if (dialogues.Count == 0)
+        {
+            if (!dialoguesFinished)
+            {
+                loadDialogues(location);
+            }
         }
-        //Conner put your fade here.
+
+        t = dialogues.Dequeue();
+        advanceDialogue(t);
+
+        if (dialogues.Count == 0)
+        {
+            dialoguesFinished = true;
+        }
+
+
+    }
+
+    private void advanceDialogue(DialogueText text)
+    {
+        if (!isDialogueOpen)
+        {
+            isDialogueOpen = true;
+        }
+        if (dialogueController.conversationEnded)
+        {
+            loadNextDialogue(l);
+        }
+        else
+        {
+            dialogueController.displayNextParagraph(text);
+        }
     }
 }
